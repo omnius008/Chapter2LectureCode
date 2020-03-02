448ae636b2fca3f892188b76cad3c7532bca19f8
omnius008 <42618636+omnius008@users.noreply.github.com>
2020-03-02 07:13:16 -0500

    Initial commit

---
+++ Chapter2LectureCode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //first assignment, make a C# .net core Console App and write something to the console, and push up to github
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
